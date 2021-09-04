f35269aa1ac7464afb3680f8327b0eb4fa6c19dd
Nauman Ansari <77458817+Nauman485@users.noreply.github.com>
2021-09-04 17:30:33 +0530

    Add files via upload

---
+++ Geekster/src/Javadsa/Addition.java
@@ -0,0 +1,22 @@
+package Javadsa;
+
+public class Addition {
+	
+	public static void main(String[] args)
+	{
+	
+	 Addition a = new Addition();
+	 a.Add();
+	
+	}
+	void Add() {
+		int num1=123;
+		int num2=147;
+		int num3;
+		num3=num1+num2;
+		
+	    System.out.println(num3);
+		}
+
+
+}
---
+++ Geekster/src/Javadsa/Minimum.java
@@ -0,0 +1,21 @@
+package Javadsa;
+
+public class Minimum {
+     
+	public static void main(String[] args)
+	{
+		int num1=123;
+		int num2=145;
+		
+		if(num1 < num2)
+		{
+			System.out.println(num1);
+		}
+		else
+		{
+			System.out.println(num2);
+		}
+		
+	}
+
+}
